02d430d2c45f6a244afdffddd80ee875acfeb51f
Carlos Roberto <carlosrjhoe@gmail.com>
2022-04-06 19:32:55 -0300

    Criando lista de categorias: Stateful e Stateless

--- ceep/src/componets/ListaCategorias/listaDeCategorias.jsx
+++ ceep/src/componets/ListaCategorias/listaDeCategorias.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-
+import "./estilo.css";
 class ListaDeCategorias extends Component {
   
+  _handleEventoInput(e) {
+    console.log(e.Key)
+    if(e.Key == "Enter"){
+      console.log("Adicionar categoria");
+    }
+  }
   render() { 
     return (
-      <section>
-        <ul>
-          <li>Categorias</li>
-          <li>Categorias</li>
-          <li>Categorias</li>
-          <li>Categorias</li>
+      <section className="lista-categorias">
+        <ul className="lista-categorias-lista">
+          <li className='lista-categorias-item'>Categorias</li>
+          <li className='lista-categorias-item'>Categorias</li>
+          <li className='lista-categorias-item'>Categorias</li>
+          <li className='lista-categorias-item'>Categorias</li>
       </ul>
-      <input type="text"/>
+      <input type="text" className='lista-categorias-input' placeholder='Adicionar Categoria' onKeyUp={this._handleEventoInput.bind(this)}/>
       </section>
     
     );
