f7c0c569038a832dc8eaeea86480809304a9e942
Carlos Roberto <carlosrjhoe@gmail.com>
2022-05-13 20:06:01 -0300

    Explorando o React Styled components

--- naruto/src/components/GlobalStyle.js
+++ naruto/src/components/GlobalStyle.js
@@ -1,8 +1,11 @@
 import { createGlobalStyle } from "styled-components";
+import bgimg from "../images/bg.jpeg";
 
 export const GlobalStyle = createGlobalStyle`
   body{
-    color: ;
+    background: url(${bgimg});
+    background-size: cover;
+    color: #332c36;
     padding: 0;
     margin: 0;
     font-family: 'New Tegomin', serif;
--- naruto/src/components/Quotes/Quotes.js
+++ naruto/src/components/Quotes/Quotes.js
@@ -1,8 +1,25 @@
-export const Quotes = () => {
+import style from "styled-components";
+
+export const Quotes = ({ quotes }) => {
   return (
-    <div>
-      <p>Carlos</p>
-      <button>Jutsu</button>
-    </div>
+    <Wrapper>
+      <Quote>
+        <p>Carlos</p>
+        <button>Jutsu</button>
+      </Quote>
+    </Wrapper>
   )
 }
+
+const Wrapper = style.div`
+  flex: 1;
+  displey: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Quote = style.p`
+  font-size: 2em;
+  flex: 1;
+  margin: 0;
+`;
