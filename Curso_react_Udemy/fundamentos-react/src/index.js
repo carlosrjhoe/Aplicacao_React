b3699d1e8f6fb2eb4060588ee8215a07adae3fba
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-22 20:14:40 -0300

    Criando um segundo Componente

--- Curso_react_Udemy/fundamentos-react/src/index.js
+++ Curso_react_Udemy/fundamentos-react/src/index.js
@@ -1,13 +1,14 @@
 import ReactDOM from "react-dom";
 import './index.css' // inportando arquivo CSS
-import Primeiro from './components/primeiro' // Importando 1° componente
+import Primeiro from './components/Primeiro' // Importando 1° componente
+import Segundo from './components/Segundo'
 
 // Sintaxe JSX
 ReactDOM.render(
     <div>
         <h1>Olá mundo, Seja bem vindo!!!</h1>
         <Primeiro></Primeiro>
-        <Primeiro></Primeiro>
+        <Segundo></Segundo>
     </div>,
     document.getElementById('root')
 )
