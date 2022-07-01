1a8c24bc0df7b49265ff3042398a68b23b56d2cd
Carlos Roberto <carlosrjhoe@gmail.com>
2022-06-30 22:01:06 -0300

    aprendendo passo a passo de function components

--- Curso_react_Udemy/fundamentos-react/src/App.jsx
+++ Curso_react_Udemy/fundamentos-react/src/App.jsx
@@ -5,8 +5,7 @@ import Imc from "./components/Imc";
 function App() {
     return (
         <div>
-            <Aleatorio  min={1} max={100}/>
-            <Imc peso={85} altura={1.81}/>          
+            <Aleatorio  min={1} max={10}/>        
         </div>
     )
 } export default App;
--- Curso_react_Udemy/fundamentos-react/src/components/Aleatorio.jsx
+++ Curso_react_Udemy/fundamentos-react/src/components/Aleatorio.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Aleatorio (props) {
     const min = props.min;
     const max = props.max;
-    const aleatorio = parseInt(Math.random() * (max - min));
+    // const { min, max } = props;
+    const aleatorio = parseInt(Math.random() * (max - min))+ min;
     return (
         <div>
             <h1>Valor Aleatório</h1>
-            <button className="botao">Aperte aqui!</button>
             <p><strong>Valor minimo: </strong> {min} </p>
             <p><strong>Valor maximo: </strong> {max} </p>
             <p><strong>Valor Escolhido: </strong> {aleatorio} </p>
