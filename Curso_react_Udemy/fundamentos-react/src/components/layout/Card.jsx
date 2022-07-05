1e2e410f4aebbaefa886b3c12396ec596705455a
Carlos Roberto <carlosrjhoe@gmail.com>
2022-07-04 21:34:35 -0300

    Adicionando comentários da adição de cores como propriedade

--- Curso_react_Udemy/fundamentos-react/src/components/layout/Card.jsx
+++ Curso_react_Udemy/fundamentos-react/src/components/layout/Card.jsx
@@ -3,11 +3,13 @@ import "./Card.css"
 
 export default (props) => {
 
+    // Criando um objeto com cores
     const cardStyle = {
         backgroundColor: props.color,
     }
 
     return (
+        // Passando o o objeto como parametro para a propriedade style.
         <div className="Card" style={cardStyle}>
             <h1 className="Title">{props.titulo}</h1> 
             <div className="Content"> 
