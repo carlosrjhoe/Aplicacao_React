98656602e7ba294a130b6d3de16e8bad4f5a1d4a
Carlos Roberto <carlosrjhoe@gmail.com>
2022-07-14 19:00:18 -0300

    Revendo o que foi visto no video anterios

--- Curso_react_Udemy/fundamentos-react/src/components/Condicional/ParOuImpar.jsx
+++ Curso_react_Udemy/fundamentos-react/src/components/Condicional/ParOuImpar.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 
-// Utilizando um function component
 function ParOuImpar(props) {
-    // Tratando o valor passado por props, vendo o resto da divisão se é = 0, se for, quer dizer que o número passado é par. Caso contrario, é impar.
     const resultado = props.numero % 2 === 0;
     return (
         <div>
-            {/* Aqui estou renderizando por condicional e utilizando o operador ternário valor do resultado se é par ou impar. */}
             { resultado ? <span>Par</span> : <span>Impar</span> }
         </div>
     )
