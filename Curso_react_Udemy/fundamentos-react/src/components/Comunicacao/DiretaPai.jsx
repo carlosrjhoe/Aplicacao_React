1177a4c7aeebbe4b04693258fde2be0ead521293
Carlos Roberto <carlosrjhoe@gmail.com>
2022-07-14 20:49:20 -0300

    Estudando comunicação direta ReactJS

---
+++ Curso_react_Udemy/fundamentos-react/src/components/Comunicacao/DiretaPai.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import DiretaFilho from "./DiretaFilho"
+
+export default props => {
+    return (
+        <div>
+            <DiretaFilho texto="Carlos" numero={36} bool={true}></DiretaFilho>
+        </div>
+    )
+}
